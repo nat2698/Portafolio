4e1cfad7eb17257073c782286ee67bb6dcca78d8
Natalia Salazar <nata.salazar2698@gmail.com>
2022-09-19 11:33:17 -0500

    cambios

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.enlaces-menu');
+const buttom_responsive = document.querySelector('.btn');
+const menu = document.querySelector('.navbar');
+const menu_items = document.querySelectorAll('.navbar a');
 
-ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
+menu_items.forEach((item) => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('responsive_active');
+        menu.classList.add('navbar');
+    });
+});
+
+buttom_responsive.addEventListener('click', () => {
+    menu.classList.toggle('responsive_active');
+    menu.classList.toggle('navbar');
+    // console.log('click');
 });
